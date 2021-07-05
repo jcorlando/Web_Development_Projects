fa3f0ac66ea805fc4b081a1a0bc75ba3f5b8cc43
jcorlando <jcorlandomc@yahoo.com>
2021-07-05 00:10:45 -0700

    Jul-05-2021 12:10 AM

--- webscraping/main.js
+++ webscraping/main.js
@@ -4,7 +4,7 @@ import yahooFinance from 'yahoo-finance2';
 
 
 const query = 'DOGE-USD';
-const queryOptions = { period1: '2021-02-01', period2: '2021-02-02' };
+const queryOptions = { period1: '2021-07-01', /* ... */ }; // From current date until "period1"
 const result = await yahooFinance.historical(query, queryOptions);
 
 
