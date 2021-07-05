c28c7968b6c4ae7d4543dae3368c04bb04e33c18
jcorlando <jcorlandomc@yahoo.com>
2021-07-05 15:43:19 -0700

    Jul-05-2021 3:43 PM

--- webscraping/main.js
+++ webscraping/main.js
@@ -1,24 +1,31 @@
 
 import yahooFinance from 'yahoo-finance2';
 
-// Construct date-time string
-const delta_date = new Date();
-// Sets the Date Delta Difference
-delta_date.setDate(delta_date.getDate() - 6);
-
-const query = 'DOGE-USD';
-const queryOptions = { period1: delta_date.toISOString().split('T')[0], /* ... */ }; // From current date until "period1"
-const result = await yahooFinance.historical(query, queryOptions);
-
-
-for(var i = 0; i < result.length; i++)
+async function getdata()
 {
-	var frame = result[i];
-	var date = frame.date;
-	var close = frame.close;
-	console.log(date, close);
-}
+	// Construct date-time string
+	const delta_date = new Date();
+	// Sets the Date Delta Difference
+	delta_date.setDate(delta_date.getDate() - 2190); // Six years is 2190 days
+	
+	const query = 'DOGE-USD';
+	const queryOptions = { period1: delta_date.toISOString().split('T')[0], /* ... */ }; // From current date until "period1"
+	const result = await yahooFinance.historical(query, queryOptions);
+	
+	
+	// Declare empty arrays for data
+	let date = [];
+	let close = [];
+	
+	for(var i = 0; i < result.length; i++)
+	{
+		date.push(result[i].date.toISOString().split('T')[0]);
+		close.push(result[i].close);
+	}
 
 
+} // End Of Async Function "getdata()"
 
+
+getdata();
 
