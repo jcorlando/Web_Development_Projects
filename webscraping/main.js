cf54dcff5017189d9b50209a6f2adb400bf381c4
jcorlando <jcorlandomc@yahoo.com>
2021-07-05 14:49:15 -0700

    Jul-05-2021 2:49 PM

--- webscraping/main.js
+++ webscraping/main.js
@@ -1,20 +1,14 @@
 
 import yahooFinance from 'yahoo-finance2';
 
-
 // Construct date-time string
 const delta_date = new Date();
+// Sets the Date Delta Difference
 delta_date.setDate(delta_date.getDate() - 6);
 
-delta_date.toISOString().split('T')[0];
-
-
-
 const query = 'DOGE-USD';
-const queryOptions = { period1: delta_date, /* ... */ }; // From current date until "period1"
+const queryOptions = { period1: delta_date.toISOString().split('T')[0], /* ... */ }; // From current date until "period1"
 const result = await yahooFinance.historical(query, queryOptions);
-
-
 
 
 for(var i = 0; i < result.length; i++)
